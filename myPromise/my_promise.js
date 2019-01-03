b4a3be265bb529fa792c8766bf8ab4b9d3072b0a
krisGooooo <shenya123@hotmail.com>
2019-01-03 22:02:41 +0800

    myPromise v2.0 add

--- myPromise/my_promise.js
+++ myPromise/my_promise.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: krisGooooo
+ * @Description: 简单的按照 Promise/A+ 规范修改
+ * @Date: 2019-01-02 21:34:29
+ * @LastEditors: krisGooooo
+ * @LastEditTime: 2019-01-03 21:58:42
+ */
+
 //  三种状态：等待 成功 失败
 const PENDING = "pending"
 const RESOLVED = 'resolved'
@@ -17,24 +25,33 @@ function MyPromise(fn) {
 
   //  resolve函数
   function resolve(value) {
-    //  只有pending状态可以改变状态
-    if (that.state === PENDING) {
-      //  切换状态
-      that.state = RESOLVED
-      //  传入的值赋给value
-      that.value = value
-      //  遍历回调数组并执行
-      that.resolvedCallbacks.map(cb => cb(that.value))
+    //  resolve函数 首先需要判断传入的值是否为 Promise 类型
+    if (value instanceof MyPromise) {
+      return value.then(resolve, reject)
     }
+    //  js event loop 保证函数执行顺序
+    setTimeout(() => {
+      //  只有pending状态可以改变状态
+      if (that.state === PENDING) {
+        //  切换状态
+        that.state = RESOLVED
+        //  传入的值赋给value
+        that.value = value
+        //  遍历回调数组并执行
+        that.resolvedCallbacks.map(cb => cb(that.value))
+      }
+    }, 0)
   }
 
   //  reject函数
   function reject(value) {
-    if (that.state === PENDING) {
-      that.state = REJECTED
-      that.value = value
-      that.rejectedCallbacks.map(cb => cb(that.value))
-    }
+    setTimeout(() => {
+      if (that.state === PENDING) {
+        that.state = REJECTED
+        that.value = value
+        that.rejectedCallbacks.map(cb => cb(that.value))
+      }      
+    }, 0)
   }
 
   //  执行Promise中传入的函数
@@ -47,6 +64,7 @@ function MyPromise(fn) {
   }
 
 }
+
 //  实现 then 函数
 MyPromise.prototype.then = function (onFulfilled, onRejected) {
   const that = this
@@ -55,20 +73,103 @@ MyPromise.prototype.then = function (onFulfilled, onRejected) {
   onRejected = typeof onRejected === 'function' ? onRejected : r => {
     throw r
   }
+  let promise2
   //  判断状态 若是pending 状态 则往回调函数中push函数
   if (that.state === PENDING) {
-    that.resolvedCallbacks.push(onFulfilled)
-    that.rejectedCallbacks.push(onRejected)
+    //  每个then函数都需要返回一个新的Promise对象，该对象用于保存新的返回对象
+    return (promise2 = new MyPromise((resolve, reject) => {
+      that.resolvedCallbacks.push(() => {
+        try {
+          //  规范规定 执行onFulfilled 或 onRejected 函数会返回一个x
+          const x = onFulfilled(that.value)
+          //  执行promise 解决过程 为了不同的promise都可以兼容使用
+          resolutionProcedure(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)          
+        }
+      })
+      
+      that.rejectedCallbacks.push(() => {
+        try {
+          const x = onRejected(that.value)
+          resolutionProcedure(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)
+        }
+      })
+    }))
   }
   //  判断状态 若不是pending 则去执行响应的函数
   if (that.state === RESOLVED) {
-    onFulfilled(that.value)
+    return (promise2 = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          const x = onFulfilled(that.value)
+          resolutionProcedure(promise2, x, resolve, reject)
+        } catch (reason) {
+          reject(reason)
+        }
+      })
+    }))
   }
   if (that.state === REJECTED) {
-    onRejected(that.value)
+    return (promise2 = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          const x = onRejected(that.value)
+          resolutionProcedure(promise2, x, resolve, reject)
+        } catch (reason) {
+          reject(reason)
+        }
+      })
+    }))
   }
 }
 
+function resolutionProcedure(promise2, x, resolve, reject) {
+  //  规定x 不能于 promise2 相等，否则会发生循环引用的问题
+  if (promise2 === x){
+    return reject(new TypeError('Error'))
+  }
+  //  判断x的类型 若x为Promise
+  //  1. 若 x 处于等待态 Promise 需保持为等待态直至 x 被执行或拒绝
+  //  2. 若 x 处于其他状态 则用相同的值处理 Promise
+  if (x instanceof MyPromise) {
+    x.then(function(value) {
+      resolutionProcedure(promise2, value, resolve, reject)
+    }, reject)
+  }
+  //  判断是否已经调用过函数
+  let called = false
+  if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      let then = x.then
+      if (typeof then === 'function') {
+        then.call(
+          x,
+          y => {
+            if (called) return
+            called = true
+            resolutionProcedure(promise2, y, resolve, reject)
+          },
+          e => {
+            if (called) return
+            called = true
+            reject(e)
+          }
+        )
+      } else {
+        resolve(x)
+      }
+    } catch (e) {
+      if (called) return
+      called = true
+      reject(e)
+    }
+  } else {
+    resolve(x)
+  }
+}
 //  例子
 new MyPromise((resolve, reject) => {
   setTimeout(() => {
